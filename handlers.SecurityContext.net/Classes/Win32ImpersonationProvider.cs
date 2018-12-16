298ff0f4a693e105767dde32cb439395f96f720a
Steve <steve.shortt@gmail.com>
2018-12-16 13:33:52 -0600

    code cleanup, class/filename alignment

--- handlers.SecurityContext.net/Classes/Win32ImpersonationProvider.cs
+++ handlers.SecurityContext.net/Classes/Win32ImpersonationProvider.cs
@@ -1,15 +1,15 @@
 ﻿using System;
-using System.Security;
-using System.Security.Principal;
 
 using Synapse.Core;
+
 using Zephyr.SecurityContext;
 using Zephyr.SecurityContext.Windows;
 
 
-public class Win32IdentityProvider : SecurityContextRuntimeBase
+#if(NET452 || NET46 || NET461 || NET462 || NET47 || NET471 || NET472)
+public class Win32ImpersonationProvider : SecurityContextRuntimeBase
 {
-    Win32Identity _w32;
+    Win32Impersonate _identity = null;
 
     public override ExecuteResult Logon(SecurityContextStartInfo startInfo)
     {
@@ -18,9 +18,10 @@ public class Win32IdentityProvider : SecurityContextRuntimeBase
             Win32LogonInfo logonInfo = DeserializeOrNew<Win32LogonInfo>( startInfo.Parameters );
             logonInfo.MakeSecure();
 
-            _w32 = new Win32Identity( logonInfo.SecureUserName, logonInfo.SecureDomain, logonInfo.SecurePassword );
+            _identity = new Win32Impersonate();
+            _identity.Impersonate( logonInfo.SecureUserName, logonInfo.SecureDomain, logonInfo.SecurePassword );
 
-            string message = $"SecurityContext: {logonInfo.SecureUserName.ToUnsecureString()}, HasIdentity: {_w32.HasIdentity}";
+            string message = $"SecurityContext: {logonInfo.SecureUserName.ToUnsecureString()}, IsImpersonating: {_identity.IsImpersonating}";
             OnLogMessage( "Logon", message );
             return new ExecuteResult() { Status = StatusType.Success, Message = message };
         }
@@ -31,22 +32,9 @@ public class Win32IdentityProvider : SecurityContextRuntimeBase
         }
     }
 
-    public override ExecuteResult ExecuteProxy(Func<HandlerStartInfo, ExecuteResult> func, HandlerStartInfo handlerStartInfo)
-    {
-        ExecuteResult result = null;
-        WindowsIdentity.RunImpersonated( _w32.Identity.AccessToken, () =>
-        {
-            result = func( handlerStartInfo );
-            result.SecurityContext = _w32.Identity.Name;
-        } );
-        return result;
-    }
-
-    public override bool UseExecuteProxy { get { return true; } set { } }
-
     public override void Logoff()
     {
-        _w32?.Logout();
+        _identity?.Undo();
     }
 
     public override object GetConfigInstance()
@@ -64,3 +52,4 @@ public class Win32IdentityProvider : SecurityContextRuntimeBase
         };
     }
 }
+#endif
---
+++ handlers.SecurityContext.net/Classes/Win32LogonInfo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Security;
+
+using Zephyr.SecurityContext;
+
+
+public class Win32LogonInfo
+{
+    public string UserName { get; set; }
+    public string Password { get; set; }
+    public string Domain { get; set; }
+
+    internal SecureString SecureUserName;
+    internal SecureString SecurePassword;
+    internal SecureString SecureDomain;
+
+    public void MakeSecure()
+    {
+        SecureUserName = UserName.ToSecureString();
+        UserName = null;
+
+        SecurePassword = Password.ToSecureString();
+        Password = null;
+
+        SecureDomain = Domain.ToSecureString();
+        Domain = null;
+    }
+}
